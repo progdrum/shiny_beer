291d9472183858594f4b3cf897d8ba3571f97d13
progdrum <brookforestscitech@gmail.com>
2017-10-08 12:27:16 -0600

    Begin the Shiny app, update gitignore, and initialize README

---
+++ README.md
@@ -0,0 +1,3 @@
+# Shiny Beer
+
+This is a Shiny app version of my ABV Kaggle [kernel](https://www.kaggle.com/progdrum/not-all-beers-are-created-equal-ly-alcoholic) 
---
+++ shiny_beer/app.R
@@ -0,0 +1,41 @@
+library(shiny)
+library(ggplot2)
+
+# Define UI for application
+ui <- fluidPage(
+   titlePanel("Beer Maps by ABV (% Alcohol By Volume"),
+   
+   # Sidebar with a slider input for number of bins 
+   sidebarLayout(
+      sidebarPanel(
+         sliderInput("bins",
+                     "Number of bins:",
+                     min = 1,
+                     max = 50,
+                     value = 30)
+      ),
+      
+      # Show a plot of the generated distribution
+      mainPanel(
+         plotOutput("distPlot")
+      )
+   )
+)
+
+# Define server logic required to draw a histogram
+server <- function(input, output) {
+   
+   output$distPlot <- renderPlot({
+      # generate bins based on input$bins from ui.R
+      x    <- faithful[, 2] 
+      bins <- seq(min(x), max(x), length.out = input$bins + 1)
+      
+      # draw the histogram with the specified number of bins
+      ggplot(data = x) + geom_histogram(bins = bins)
+      # hist(x, breaks = bins, col = 'darkgray', border = 'white')
+   })
+}
+
+# Run the application 
+shinyApp(ui = ui, server = server)
+
